9ade573b3015b495fc190c5a00ffd11a7eefbdc1
SonHyunHo <engel0320@gmail.com>
2015-03-18 11:11:35 +0900

    테스트클래스 첫 버전

---
+++ CharacterStackTest/src/CharacterStackTest.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class CharacterStackTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCharacterStack() {
+		fail("Not yet implemented");
+	}
+
+}
